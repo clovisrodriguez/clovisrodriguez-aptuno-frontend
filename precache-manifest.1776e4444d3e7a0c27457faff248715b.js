645170d683e52da024cbe7a3259c50c81f77a2df
clovis <clovis.rodriguez@qatro20.com>
2020-06-16 19:35:37 -0500

    Updates

---
+++ precache-manifest.1776e4444d3e7a0c27457faff248715b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa12ec3ade363eae0a59053d3fc5257f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7ae9b7b7413dabea7f2",
+    "url": "/static/css/2.76c105bc.chunk.css"
+  },
+  {
+    "revision": "e7ae9b7b7413dabea7f2",
+    "url": "/static/js/2.c8c5b68d.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.c8c5b68d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e0e112aa9dbda65732",
+    "url": "/static/js/main.dd287f78.chunk.js"
+  },
+  {
+    "revision": "26ce9cd0656e08722d41",
+    "url": "/static/js/runtime-main.bd501350.js"
+  },
+  {
+    "revision": "f64d22fdc9aa48f27d81088135098aae",
+    "url": "/static/media/aptuno_logo_2colors.f64d22fd.svg"
+  },
+  {
+    "revision": "7d08a5064086375a614239ba970633bf",
+    "url": "/static/media/not-available-list.7d08a506.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clovisrodriguez-aptuno-frontend/precache-manifest.0444853e93ff3da7bce783ce23209d06.js"
+  "/precache-manifest.1776e4444d3e7a0c27457faff248715b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clovisrodriguez-aptuno-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
